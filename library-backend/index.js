f285d20b0c7dbb550bfe24f4ed3f3b6568c5ca0d
Student <student@sirafrica.org>
2026-08-04 10:11:19 +0300

    8.7: add editAuthor mutation

--- library-backend/index.js
+++ library-backend/index.js
@@ -108,6 +108,7 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -144,6 +145,18 @@ const resolvers = {
       }
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) =>
+        a.name === args.name ? updatedAuthor : a
+      )
+      return updatedAuthor
     },
   },
 }
