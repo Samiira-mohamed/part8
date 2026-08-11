e4bda648169dac30b5823e5af82c682c96bd9ed2
Student <student@sirafrica.org>
2026-08-11 11:05:18 +0300

    19: implement login functionality and fix mutations

--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -31,3 +31,34 @@ export const EDIT_AUTHOR = gql`
     }
   }
 `
+
+export const CREATE_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author {
+        name
+      }
+      published
+      genres
+    }
+  }
+`
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`
